0b6872d04c1de96d103384b48416de469be1b883
DreamWorld420 <dreamworld420@protonmail.com>
2022-10-22 20:25:37 +0330

    dep: add tailwindcss

---
+++ components/ui/Hero.tsx
@@ -0,0 +1,14 @@
+import type { FC } from "react";
+
+const Hero: FC = () => {
+	return (
+		<div id="hero">
+			<h1>
+				I&apos;m Kasra - a frontend web developer, maker of digital tool
+				and toys.
+			</h1>
+		</div>
+	);
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
+import Hero from "../components/ui/Hero";
 
 const Homepage: NextPage = () => {
-	return null;
+	return (
+		<section className="flex flex-col h-screen">
+			<main className="grow flex flex-col justify-center">
+				<Hero />
+			</main>
+		</section>
+	);
 };
 
 export default Homepage;
