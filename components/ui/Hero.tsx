05d6bc8eeb554497dc105f7af49ec59704f5c159
DreamWorld420 <dreamworld420@protonmail.com>
2022-10-22 20:28:32 +0330

    feat: add

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -3,9 +3,10 @@ import type { FC } from "react";
 const Hero: FC = () => {
 	return (
 		<div id="hero">
-			<h1>
-				I&apos;m Kasra - a frontend web developer, maker of digital tool
-				and toys.
+			<h1 className="font-openSans text-xl leading-7">
+				I&apos;m Kasra - a frontend web developer
+				<br />
+				maker of digital tool and toys.
 			</h1>
 		</div>
 	);
