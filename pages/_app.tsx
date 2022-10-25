815a3aa16c82479cac58c5084c93adde73cad317
DreamWorld420 <dreamworld420@protonmail.com>
2022-10-26 03:25:28 +0330

    chore: meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import PageLayout from "../components/layout/PageLayout";
 import "../styles/globals.css";
@@ -9,21 +10,66 @@ import SocialButtonGroup from "../components/ui/SocialButtonGroup";
 
 function App({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider theme={theme}>
-			<PageLayout
-				maxW="container.lg"
-				className="h-screen flex flex-col justify-between"
-			>
-				<Navigation>
-					<NavLink href="/">Home</NavLink>
-					<NavLink href="/projects">Work</NavLink>
-				</Navigation>
-				<Component {...pageProps} />
-				<footer className="mb-4 flex flex-row justify-end">
-					<SocialButtonGroup />
-				</footer>
-			</PageLayout>
-		</ChakraProvider>
+		<>
+			<Head>
+				{/* html */}
+				<title>Kasra | DreamWorld420</title>
+				<meta
+					name="description"
+					content="Kasra | DreamWorld420 portfolio website"
+				/>
+
+				{/* facebook */}
+				<meta
+					property="og:title"
+					content="Kasra | DreamWorld420"
+					key="title"
+				/>
+				<meta
+					property="og:description"
+					content="Kasra | DreamWorld420 portfolio website"
+				/>
+				<meta
+					property="og:image"
+					content="https://www.dreamworld420.com/me.webp"
+				/>
+				<meta
+					property="og:url"
+					content="https://www.dreamworld420.com"
+				/>
+
+				{/* twitter */}
+				<meta name="twitter:title" content="Kasra | DreamWorld420" />
+				<meta
+					name="twitter:description"
+					content="Kasra | DreamWorld420 portfolio website"
+				/>
+				<meta
+					name="twitter:image"
+					content="https://www.dreamworld420.com/me.webp"
+				/>
+				<meta
+					property="twitter:url"
+					content="https://www.dreamworld420.com"
+				/>
+				<meta property="twitter:domain" content="dreamworld420.com" />
+			</Head>
+			<ChakraProvider theme={theme}>
+				<PageLayout
+					maxW="container.lg"
+					className="h-screen flex flex-col justify-between"
+				>
+					<Navigation>
+						<NavLink href="/">Home</NavLink>
+						<NavLink href="/projects">Work</NavLink>
+					</Navigation>
+					<Component {...pageProps} />
+					<footer className="mb-4 flex flex-row justify-end">
+						<SocialButtonGroup />
+					</footer>
+				</PageLayout>
+			</ChakraProvider>
+		</>
 	);
 }
 
